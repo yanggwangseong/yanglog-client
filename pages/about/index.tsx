34bcd07c6f6042712c82d7efd60bce4248e4a892
GwangSeong Yang <rhkdtjd_12@naver.com>
2022-07-19 22:08:09 +0900

    feat : next.js tailwind install

---
+++ pages/about/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const About: NextPage = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+    );
+}
+
+export default About;
