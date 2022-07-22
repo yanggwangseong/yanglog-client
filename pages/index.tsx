a8952bc6b625d07dddb2da6110ea6e090f8871dc
YangGwangSeong <rhkdtjd_12@naver.com>
2022-07-22 07:20:42 -0700

    feat : add section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import Format from '../layout/format'
 
+//components
+import Section1 from '../components/section/section1'
 const Home: NextPage = () => {
   return (
     <div>
       <Format>
-        <h1>test</h1>
+        <Section1></Section1>
       </Format>
     </div>
   )
