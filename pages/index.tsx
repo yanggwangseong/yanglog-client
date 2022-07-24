98ea430c9f55b1ffd499bc28049af686ac5f9e6d
YangGwangSeong <rhkdtjd_12@naver.com>
2022-07-24 07:25:00 -0700

    feat : 최신게시물 section2 작업완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,13 @@ import Format from '../layout/format'
 
 //components
 import Section1 from '../components/section/section1'
+import Section2 from '../components/section/section2'
 const Home: NextPage = () => {
   return (
     <div>
       <Format>
         <Section1></Section1>
+        <Section2></Section2>
       </Format>
     </div>
   )
